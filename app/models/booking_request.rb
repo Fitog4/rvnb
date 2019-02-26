1206d55c848c436d01f7313e64a2add124c71285
Filippo Tognacci <fitog4@hotmail.it>
2019-02-26 15:42:17 +0100

    implement BookingRequest model, with validations and associations

---
+++ app/models/booking_request.rb
@@ -0,0 +1,20 @@
+class BookingRequest < ApplicationRecord
+  belongs_to :rv
+  belongs_to :user
+
+  validates :status, inclusion: { in: ['pending', 'approved', 'paid'] }
+  validates :location, :date_from, :date_till, presence: true
+  validate :valid_dates?
+
+  private
+
+  def valid_dates?
+    # date_till >= date_from >= Date.today
+    unless date_till >= date_from
+      errors.add(:date_till, 'Booking end date must be after booking start date')
+    end
+    unless date_from >= Date.today
+      errors.add(:date_from, 'Booking start date must today or in the future')
+    end
+  end
+end

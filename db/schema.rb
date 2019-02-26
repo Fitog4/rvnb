386827817c51c3d2ae85a095eff6e94e2f06204c
Laura Meyer <laura-meyer@live.de>
2019-02-26 16:51:52 +0100

    form started

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_110514) do
+ActiveRecord::Schema.define(version: 2019_02_26_114302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_02_26_110514) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rvs", "users"
 end

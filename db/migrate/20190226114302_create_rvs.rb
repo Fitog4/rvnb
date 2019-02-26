3f7da47774982e2bd37867f970458fa7c37b40b8
Jonah Rice <jonah@jonahrice.co.za>
2019-02-26 12:45:16 +0100

    Fixed the rv to rvs issue

--- db/migrate/20190226114302_create_rvs.rb
+++ db/migrate/20190226114302_create_rvs.rb
@@ -2,7 +2,7 @@ class CreateRvs < ActiveRecord::Migration[5.2]
   def change
     create_table :rvs do |t|
       t.string :category
-      t.references :user
+      t.references :user, foreign_key: true
 
       t.timestamps
     end

077adaad675ebef5f067d8833bcc64ca16f72a82
Laura Meyer <laura-meyer@live.de>
2019-02-28 17:42:03 +0100

    dashboard action implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
+require 'date'
+
 class PagesController < ApplicationController
   def home
     @rv = Rv.new
   end
 
   def dashboard
-    @booking_requests = BookingRequest.all
-
+  @booking_requests = BookingRequest.all.select { |request| request.date_till >= Date.today }
   end
 
   def dashboard_previous
-    render :dashboard
+  @booking_requests = BookingRequest.all.select { |request| request.date_till < Date.today }
+  render :dashboard
   end
+
 end
